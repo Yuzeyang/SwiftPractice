1d748119f3eec3340a1698372719885462677dab
yuzeyang <ios.yuzeyang@gmail.com>
2017-05-23 14:46:31 +0800

    add a shell

---
+++ DouBanMovie/DouBanMovie/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  DouBanMovie
+//
+//  Created by 宫城 on 2017/5/22.
+//  Copyright © 2017年 宫城. All rights reserved.
+//
+
+import Cocoa
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DouBanMovie/DouBanMovie/DBMainWindow.swift
@@ -0,0 +1,25 @@
+//
+//  DBMainWindow.swift
+//  DouBanMovie
+//
+//  Created by 宫城 on 2017/5/23.
+//  Copyright © 2017年 宫城. All rights reserved.
+//
+
+import Cocoa
+
+class DBMainWindow: NSWindowController {
+    let windowSize = CGSize(width: 900, height: 650)
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        if let window = window, let screen = window.screen {
+            let screenFrame = screen.visibleFrame
+            let x = screenFrame.midX - windowSize.width/2
+            let y = screenFrame.midY - windowSize.height/2
+            let origin = CGPoint(x: x, y: y)
+            window.setFrame(NSRect(origin: origin, size: windowSize), display: true)
+        }
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+}
